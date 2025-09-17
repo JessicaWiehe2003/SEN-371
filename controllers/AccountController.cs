37013caf724438fe41ea1b95b4c76aad2c13e2f5
Thunder812 <arnan.kruger@gmail.com>
2025-09-17 13:41:36 +0200

    Add files via upload
    First steps to adding GUI interface design code. More controllers need to bee added later

---
+++ controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Project_4_CampusLearn.Data;
+using Project_4_CampusLearn.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Project_4_CampusLearn.ViewModels.AuthVms;
+
+namespace Project_4_CampusLearn.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _users;
+        private readonly SignInManager<AppUser> _signIn;
+
+        public AccountController(UserManager<AppUser> users, SignInManager<AppUser> signIn)
+        { _users = users; _signIn = signIn; }
+
+        [HttpGet] public IActionResult Login() => View();
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVm vm)
+        {
+            var user = await _users.FindByEmailAsync(vm.Email);
+            if (user == null) { ModelState.AddModelError("", "Invalid"); return View(vm); }
+            var result = await _signIn.PasswordSignInAsync(user, vm.Password, true, true);
+            if (!result.Succeeded) { ModelState.AddModelError("", "Invalid"); return View(vm); }
+            return Redirect("/topics");
+        }
+
+        [HttpGet] public IActionResult Register() => View();
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVm vm)
+        {
+            if (!ModelState.IsValid) return View(vm);
+            var u = new AppUser { UserName = vm.Email, Email = vm.Email, FullName = vm.FullName, IsTutor = vm.IsTutor };
+            var r = await _users.CreateAsync(u, vm.Password);
+            if (!r.Succeeded) { foreach (var e in r.Errors) ModelState.AddModelError("", e.Description); return View(vm); }
+            await _signIn.SignInAsync(u, true);
+            return Redirect("/topics");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signIn.SignOutAsync();
+            return Redirect("/");
+        }
+    }
+}
---
+++ controllers/ChatPageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_4_CampusLearn.Controllers
+{
+    [Authorize]
+    public class ChatPageController : Controller
+    {
+        [HttpGet("/ChatPage")]
+        public IActionResult Index(int topicId) { ViewBag.TopicId = topicId; return View(); }
+    }
+}
