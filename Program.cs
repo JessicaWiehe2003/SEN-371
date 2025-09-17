3cdc65dbf237a6a350bfba211f13917d2deb1c05
Thunder812 <arnan.kruger@gmail.com>
2025-09-17 14:00:56 +0200

    Add files via upload
    Added main Program.cs file as well as Data folder containing AppUser.cs file, DTO folder containing AuthDtos.cs file and Services folder containing JwtServices.cs file, the rest of the files will be added along with code later on.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_4_CampusLearn.Data;
+using Project_4_CampusLearn.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace Project_4_CampusLearn
+{
+    internal class Program
+    {
+        
+    }
+}
