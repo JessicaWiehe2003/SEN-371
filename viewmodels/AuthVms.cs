393ec596325b248b66dc1601d0c307586127d57d
Thunder812 <arnan.kruger@gmail.com>
2025-09-17 13:43:50 +0200

    Add files via upload
    View files added, mostly the cshtml files as well as the files for the viewmodels.

---
+++ viewmodels/AuthVms.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_4_CampusLearn.ViewModels
+{
+    internal class AuthVms
+    {
+        public class LoginVm
+        {
+            [Required, EmailAddress] public string Email { get; set; } = "";
+            [Required, DataType(DataType.Password)] public string Password { get; set; } = "";
+        }
+
+        public class RegisterVm
+        {
+            [Required] public string FullName { get; set; } = "";
+            [Required, EmailAddress] public string Email { get; set; } = "";
+            [Required, DataType(DataType.Password)] public string Password { get; set; } = "";
+            public bool IsTutor { get; set; }
+        }
+    }
+}
